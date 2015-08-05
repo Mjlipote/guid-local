0f3a33034b7e1741c6e91bd28409a165687394bd
Ming-Jheng Li <mjli@ym.edu.tw>
2015-08-05 16:13:24 +0800

    拿掉登入註解

--- src/main/java/tw/edu/ym/lab525/web/guidlocalserver/WebSecurityConfig.java
+++ src/main/java/tw/edu/ym/lab525/web/guidlocalserver/WebSecurityConfig.java
@@ -45,6 +45,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
     auth.authenticationProvider(customAuthenticationProvider);
-    // auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
   }
 }
