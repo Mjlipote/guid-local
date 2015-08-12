630338d39cd0b3f370eff86df5832288e2a15e49
Ming-Jheng Li <mjli@ym.edu.tw>
2015-08-12 16:12:01 +0800

    Add LegacyControllerTest

---
+++ src/test/java/tw/edu/ym/lab525/web/guidlocalserver/controllers/LegacyControllerTest.java
@@ -0,0 +1,118 @@
+/**
+ *
+ * @author Ming Jheng Li
+ *
+ *
+ * Copyright 2015 Ming Jheng Li
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+package tw.edu.ym.lab525.web.guidlocalserver.controllers;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import tw.guid.local.Application;
+import tw.guid.local.config.RestfulConfig;
+import tw.guid.local.helper.HttpActionHelper;
+import tw.guid.local.models.Action;
+import tw.guid.local.models.SubprimeGuidRequest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebIntegrationTest
+public class LegacyControllerTest {
+
+  @Test
+  public void testCreateSubprimeGuid() throws URISyntaxException, IOException {
+
+    List<String> guidHashList = newArrayList();
+    guidHashList.add(
+        "f3042960fc9351d1ad99550817f892968207c6cb2539c6fd11b3258e815dedfe4f8f3f2a95c846b32aacf6201282921e2b93812587cc19752cfc9c0cf236a57b00");
+    guidHashList.add(
+        "e92e7cf25a726bb9f7aff7c36c31fa4a96b0014a3a7ce5018c6b84bc459df512653253d01e0742878ca7ddd7bd9c5179273fa915761a9ba84948fd85007cc8f900");
+    guidHashList.add(
+        "636ce21c211c33e6ee8e2f7590034fef8a3a5b3263c6d83af9c54b490175d649f11937e855509f57c986d1882cb5259372a37697899660afff8db6c8049de6a900");
+
+    SubprimeGuidRequest scr = new SubprimeGuidRequest();
+    scr.setPrefix("TEST");
+    scr.setGuidHash(guidHashList);
+
+    List<SubprimeGuidRequest> spGuidCreateRequestList = newArrayList();
+    spGuidCreateRequestList.add(scr);
+
+    assertEquals(
+        HttpActionHelper.toPost(new URI(RestfulConfig.GUID_LOCAL_SERVER_URL),
+            Action.CREATE, spGuidCreateRequestList, false).getBody(),
+        "TEST-Y3XZU2NG");
+
+    // HttpClientHelper httpClientHelper = new HttpClientHelper(new
+    // URI("http://localhost:8080"), "admin", "password");
+    //
+    // assertEquals(httpClientHelper.toPost(new
+    // Gson().toJson(spGuidCreateRequestList), "create"), "TEST-Y3XZU2NG");
+
+  }
+
+  // @Test
+  // public void testBatchSubprimeGuid()
+  // throws JsonProcessingException, URISyntaxException {
+  //
+  // List<String> guidHashList = newArrayList();
+  // List<String> guidHashList1 = newArrayList();
+  // guidHashList.add(
+  // "f3042960fc9351d1ad99550817f892968207c6cb2539c6fd11b3258e815dedfe4f8f3f2a95c846b32aacf6201282921e2b93812587cc19752cfc9c0cf236a57b00");
+  // guidHashList.add(
+  // "e92e7cf25a726bb9f7aff7c36c31fa4a96b0014a3a7ce5018c6b84bc459df512653253d01e0742878ca7ddd7bd9c5179273fa915761a9ba84948fd85007cc8f900");
+  // guidHashList.add(
+  // "636ce21c211c33e6ee8e2f7590034fef8a3a5b3263c6d83af9c54b490175d649f11937e855509f57c986d1882cb5259372a37697899660afff8db6c8049de6a900");
+  //
+  // guidHashList1.add(
+  // "4cfba4fd5fb65d523c05d7f3d6ae841cb65267e7881621e84a54abe9b81f1951ce3e57438b3d5672052b274c7a34bc55e1c67639242783957b9c6d3d0f9dbfbd00");
+  // guidHashList1.add(
+  // "f02cdfc2147cc0f56c176624e73ce6ad7766f2717f56ce6c5c5827ce145f32c8a2cf8f43135b1d9517e45f31fcba814cb36c03544e253fc340ce4a477b5ec96600");
+  // guidHashList1.add(
+  // "5d8fdc76d49b1e8c3668ce98f4ace74a53838535df0f82290158b487ce81de9c3f9f4d7cb8ebf94b1fe608c49b75700109396d4fd077148bd77aee11d52b013d00");
+  //
+  // SubprimeGuidRequest scr = new SubprimeGuidRequest();
+  // scr.setPrefix("TEST");
+  // scr.setGuidHash(guidHashList);
+  //
+  // SubprimeGuidRequest scr1 = new SubprimeGuidRequest();
+  // scr1.setPrefix("TEST");
+  // scr1.setGuidHash(guidHashList1);
+  //
+  // List<SubprimeGuidRequest> spGuidCreateRequestList = newArrayList();
+  // spGuidCreateRequestList.add(scr);
+  // spGuidCreateRequestList.add(scr1);
+  //
+  // assertEquals(
+  // HttpActionHelper.toPost(new URI(RestfulConfig.GUID_LOCAL_SERVER_URL),
+  // Action.BATCH, spGuidCreateRequestList, true).getBody(),
+  // "[" + "TEST-Y3XZU2NG" + "," + "TEST-WDVWU2EQ" + "]");
+  //
+  // }
+
+}
