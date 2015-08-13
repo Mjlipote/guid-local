ce11cff0d61c90a77a72f4bc9f4f6f5762b34b51
Ming-Jheng Li <mjli@ym.edu.tw>
2015-08-13 15:37:50 +0800

    Modify security config

--- src/main/java/tw/guid/local/WebSecurityConfig.java
+++ src/main/java/tw/guid/local/WebSecurityConfig.java
@@ -38,14 +38,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    // http.authorizeRequests().antMatchers("/register", "/comparison")
-    // .hasAuthority("ROLE_ADMIN").antMatchers("/*", "/guid/web/**")
-    // .hasAnyAuthority("ROLE_ADMIN", "ROLE_USER").anyRequest().permitAll()
-    // .and().formLogin().loginPage("/login").permitAll().and().logout()
-    // .permitAll().and().csrf().disable();
+    http.authorizeRequests().antMatchers("/register", "/comparison")
+        .hasAuthority("ROLE_ADMIN").antMatchers("/*")
+        .hasAnyAuthority("ROLE_ADMIN", "ROLE_USER").antMatchers("/guid/api/*")
+        .permitAll().and().formLogin().loginPage("/login").permitAll().and()
+        .logout().permitAll().and().csrf().disable();
 
-    http.authorizeRequests().anyRequest().authenticated().and().csrf().disable()
-        .httpBasic();
+    // http.authorizeRequests().anyRequest().permitAll().and().csrf().disable()
+    // .httpBasic();
   }
 
   @Autowired
