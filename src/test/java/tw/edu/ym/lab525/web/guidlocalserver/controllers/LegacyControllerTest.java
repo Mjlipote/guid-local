0e5b6658fc0107eb0a6a4b3d2a4cf91e42c2f42e
Ming-Jheng Li <mjli@ym.edu.tw>
2015-08-15 19:08:16 +0800

    Modify LagacyControllerTest

--- src/test/java/tw/edu/ym/lab525/web/guidlocalserver/controllers/LegacyControllerTest.java
+++ src/test/java/tw/edu/ym/lab525/web/guidlocalserver/controllers/LegacyControllerTest.java
@@ -88,7 +88,7 @@ public class LegacyControllerTest {
     PII pii = new PII.Builder(new Name("明政", "李"), Sex.MALE,
         new Birthday(1979, 7, 21), new TWNationalId("E122371585")).build();
 
-    assertEquals(gc.create(pii), "AdminTest-T8A37BPL");
+    assertEquals(gc.create(pii), "AdminTest-8LO31HIK");
 
     // List<String> jsonHashes = newArrayList();
     // jsonHashes.add(hc1);
