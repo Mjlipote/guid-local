037c582407384ae6c4e8a07696aec0a591d5957e
wmw <wnameless@gmail.com>
2015-09-03 18:41:56 +0800

    Remove redundant codes

--- src/main/java/tw/guid/local/helper/HttpActionHelper.java
+++ src/main/java/tw/guid/local/helper/HttpActionHelper.java
@@ -20,6 +20,8 @@
  */
 package tw.guid.local.helper;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 import java.net.URI;
 import java.nio.charset.Charset;
 import java.util.Arrays;
@@ -28,7 +30,6 @@ import org.apache.commons.codec.binary.Base64;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -49,81 +50,46 @@ public final class HttpActionHelper {
 
   private HttpActionHelper() {}
 
-  /**
-   * 
-   * @param url
-   * @param object
-   * @param action
-   * @return
-   * @throws JsonProcessingException
-   */
   public static ResponseEntity<String> toPostWithoutApiRoot(URI url,
       Action action, Object object, boolean authority)
           throws JsonProcessingException {
-
     return PostCreater(url, action, object, authority, false);
   }
 
-  /**
-   * 
-   * @param url
-   * @param action
-   * @param object
-   * @return
-   */
   public static ResponseEntity<String> toGetWithoutApiRoot(URI url,
       Action action, String param, boolean authority) {
-
     return getCreater(url, action, param, authority, false);
   }
 
-  /**
-   * 
-   * @param url
-   * @param object
-   * @param action
-   * @return
-   * @throws JsonProcessingException
-   */
   public static ResponseEntity<String> toPost(URI url, Action action,
       Object object, boolean authority) throws JsonProcessingException {
 
     return PostCreater(url, action, object, authority, true);
   }
 
-  /**
-   * 
-   * @param url
-   * @param action
-   * @param object
-   * @return
-   */
   public static ResponseEntity<String> toGet(URI url, Action action,
       String param, boolean authority) {
-
     return getCreater(url, action, param, authority, true);
   }
 
   private static ResponseEntity<String> PostCreater(URI url, Action action,
       Object object, boolean authority, boolean apiRoot)
           throws JsonProcessingException {
-
     if (authority == true) {
       headers = getHeaders("admin", HashcodeCreator.getSha512("password"));
     } else {
-      headers.setContentType(MediaType.APPLICATION_JSON);
-      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+      headers.setContentType(APPLICATION_JSON);
+      headers.setAccept(Arrays.asList(APPLICATION_JSON));
       headers.add("custom", "true");
     }
 
-    String http[] = url.toString().split("://");
-
     HttpEntity<String> jsonRequest =
-        new HttpEntity<String>(mapper.writeValueAsString(object), headers);
+        new HttpEntity<>(mapper.writeValueAsString(object), headers);
     String urlStr = apiRoot == true
-        ? http[0] + "://" + url.getHost() + ":" + url.getPort() + "/" + API_ROOT
-            + "/" + action
-        : http[0] + "://" + url.getHost() + ":" + url.getPort() + "/" + action;
+        ? url.getScheme() + "://" + url.getHost() + ":" + url.getPort() + "/"
+            + API_ROOT + "/" + action
+        : url.getScheme() + "://" + url.getHost() + ":" + url.getPort() + "/"
+            + action;
     ResponseEntity<String> response = restTemplate.exchange(urlStr,
         HttpMethod.POST, jsonRequest, String.class);
 
@@ -132,26 +98,22 @@ public final class HttpActionHelper {
 
   private static ResponseEntity<String> getCreater(URI url, Action action,
       String param, boolean authority, boolean apiRoot) {
-
     if (authority == true) {
       headers = getHeaders("admin", HashcodeCreator.getSha512("password"));
     } else {
-      headers.setContentType(MediaType.APPLICATION_JSON);
-      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+      headers.setContentType(APPLICATION_JSON);
+      headers.setAccept(Arrays.asList(APPLICATION_JSON));
       headers.add("custom", "true");
     }
 
-    String http[] = url.toString().split("://");
-
-    HttpEntity<String> jsonRequest = new HttpEntity<String>(headers);
-    ResponseEntity<String> response =
-        restTemplate.exchange(
-            apiRoot == true
-                ? http[0] + "://" + url.getHost() + ":" + url.getPort() + "/"
-                    + API_ROOT + "/" + action + param
-                : http[0] + "://" + url.getHost() + ":" + url.getPort() + "/"
-                    + action + param,
-            HttpMethod.GET, jsonRequest, String.class);
+    HttpEntity<String> jsonRequest = new HttpEntity<>(headers);
+    ResponseEntity<String> response = restTemplate.exchange(
+        apiRoot == true
+            ? url.getScheme() + "://" + url.getHost() + ":" + url.getPort()
+                + "/" + API_ROOT + "/" + action + param
+            : url.getScheme() + "://" + url.getHost() + ":" + url.getPort()
+                + "/" + action + param,
+        HttpMethod.GET, jsonRequest, String.class);
 
     return response;
   }
@@ -164,8 +126,8 @@ public final class HttpActionHelper {
    */
   private static HttpHeaders getHeaders(String username, String password) {
     HttpHeaders httpHeaders = new HttpHeaders();
-    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-    httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+    httpHeaders.setContentType(APPLICATION_JSON);
+    httpHeaders.setAccept(Arrays.asList(APPLICATION_JSON));
     String plainCreds = username + ":" + password;
     byte[] base64CredsBytes =
         Base64.encodeBase64(plainCreds.getBytes(Charset.forName("US-ASCII")));
