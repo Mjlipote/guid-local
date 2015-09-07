ff1b83508606d2df54f14f8915c4a1436aa3203f
Ming-Jheng Li <mjli@ym.edu.tw>
2015-09-08 00:37:33 +0800

    Modify MainControllerTest

--- src/test/java/tw/guid/local/controller/MainControllerTest.java
+++ src/test/java/tw/guid/local/controller/MainControllerTest.java
@@ -20,12 +20,15 @@
  */
 package tw.guid.local.controller;
 
-import static org.junit.Assert.assertEquals;
+import static com.google.common.collect.Sets.newHashSet;
+import static org.junit.Assert.assertTrue;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -37,6 +40,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.wnameless.json.flattener.JsonFlattener;
 
 import tw.guid.local.Application;
 import tw.guid.local.helper.HttpActionHelper;
@@ -66,17 +70,56 @@ public class MainControllerTest {
         "test-deb7e9e9", "YM-9aca2291", "YM-69e4ef00", "NTU-741fa23b",
         "test-2d992478", "NTU-87316faa", "YM-e4ff7bf0", "test-a082b088");
 
-    assertEquals(
-        HttpActionHelper
-            .toPost(new URI(centralServerUrl), Action.COMPARISON, list, false)
-            .getBody(),
-        ("[" + "[\"BBB-SG1XQETJ\",\"BBN-TPW7F2KM\",\"BBO-SJ6UO7NT\"]" + ","
-            + "[\"test-a082b088\",\"YM-e4ff7bf0\"]" + ","
-            + "[\"YM-69e4ef00\",\"test-2d992478\",\"NTU-87316faa\"]" + ","
-            + "[\"NTU-9b6bb15c\",\"YM-75be31f6\",\"test-31c3fa8e\"]" + ","
-            + "[\"BAK-RAML0ATR\",\"BAP-J36E0YUY\",\"BAA-TC00G36U\"]" + ","
-            + "[\"NTU-741fa23b\",\"test-deb7e9e9\",\"YM-9aca2291\"]" + "]"));
+    Set<String> set0 = newHashSet();
+    set0.add("BBB-SG1XQETJ");
+    set0.add("BBN-TPW7F2KM");
+    set0.add("BBO-SJ6UO7NT");
+    Set<String> set1 = newHashSet();
+    set1.add("test-a082b088");
+    set1.add("YM-e4ff7bf0");
+    Set<String> set2 = newHashSet();
+    set2.add("YM-69e4ef00");
+    set2.add("test-2d992478");
+    set2.add("NTU-87316faa");
+    Set<String> set3 = newHashSet();
+    set3.add("NTU-9b6bb15c");
+    set3.add("YM-75be31f6");
+    set3.add("test-31c3fa8e");
+    Set<String> set4 = newHashSet();
+    set4.add("BAK-RAML0ATR");
+    set4.add("BAP-J36E0YUY");
+    set4.add("BAA-TC00G36U");
+    Set<String> set5 = newHashSet();
+    set5.add("NTU-741fa23b");
+    set5.add("test-deb7e9e9");
+    set5.add("YM-9aca2291");
 
+    Set<Set<String>> ssts = newHashSet();
+    ssts.add(set0);
+    ssts.add(set1);
+    ssts.add(set2);
+    ssts.add(set3);
+    ssts.add(set4);
+    ssts.add(set5);
+
+    Map<String, Object> flattenJson =
+        JsonFlattener.flattenAsMap(HttpActionHelper
+            .toPost(new URI(centralServerUrl), Action.COMPARISON, list, false)
+            .getBody());
+
+    Set<Set<String>> sets = newHashSet();
+
+    for (int i = 0; i < flattenJson.size(); i++) {
+      Set<String> set = newHashSet();
+      for (int j = 0; j < flattenJson.size(); j++) {
+        if (flattenJson.get("[" + i + "]" + "[" + j + "]") != null) {
+          set.add(flattenJson.get("[" + i + "]" + "[" + j + "]").toString());
+        }
+      }
+      if (set.size() > 0) sets.add(set);
+    }
+
+    assertTrue(sets.containsAll(ssts));
   }
 
 }
