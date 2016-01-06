4a2250c6871face59f734cb03134553881163b99
Ming-Jheng Li <mjli@ym.edu.tw>
2016-01-06 18:50:51 +0800

    Add ReiMed copyright header

--- src/main/java/tw/guid/local/web/LegacyGuidException.java
+++ src/main/java/tw/guid/local/web/LegacyGuidException.java
@@ -1,24 +1,9 @@
-/*
- *
- * @author Wei-Ming Wu
- *
- *
- * Copyright 2014 Wei-Ming Wu
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- *
- */
-
+/* Copyright (c) 2015 ReiMed Co. to present.
+* All rights reserved.
+*
+* @author Wei-Ming Wu
+*
+*/
 package tw.guid.local.web;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -32,8 +17,8 @@ import com.google.common.collect.ImmutableList;
 
 /**
  * 
- * {@link LegacyGuidException} is used to represent any exception happened in the GUID
- * web service.
+ * {@link LegacyGuidException} is used to represent any exception happened in
+ * the GUID web service.
  *
  */
 public final class LegacyGuidException extends RuntimeException {
@@ -51,7 +36,8 @@ public final class LegacyGuidException extends RuntimeException {
    * @param errorMessages
    *          of exception details
    */
-  public LegacyGuidException(HttpStatus httpStatus, List<String> errorMessages) {
+  public LegacyGuidException(HttpStatus httpStatus,
+      List<String> errorMessages) {
     this.httpStatus = checkNotNull(httpStatus);
     this.errorMessages = ImmutableList.copyOf(errorMessages);
   }
