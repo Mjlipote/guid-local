c25023e99fe89dfc4b2713433094e60a464131c7
Ming-Jheng Li <mjli0721@gmail.com>
2016-01-07 17:22:35 +0800

    Modify ApiControllerTest

--- src/test/java/tw/guid/local/controller/ApiControllerTest.java
+++ src/test/java/tw/guid/local/controller/ApiControllerTest.java
@@ -10,24 +10,35 @@ import static com.google.common.collect.Sets.newHashSet;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.github.wnameless.json.flattener.JsonFlattener;
+import com.github.wnameless.workbookaccessor.WorkbookReader;
 
 import tw.guid.local.Application;
-import tw.guid.local.helper.HttpActionHelper;
+import tw.guid.local.repository.SubprimeGuidRepository;
 import tw.guid.local.web.Action;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -38,27 +49,77 @@ public class ApiControllerTest {
   @Autowired
   Environment env;
 
-  @Value("${guid.local.server.url}")
-  String localServerUrl;
+  @Autowired
+  SubprimeGuidRepository spGuidRepo;
 
-  @Test
-  public void testValidationSubprimeGuidLength() throws URISyntaxException {
+  String localServerUrl = "https://localhost:8443";
 
-    assertTrue(
-        HttpActionHelper.toGet(new URI(localServerUrl), Action.API_VALIDATION,
-            "?spguid=" + "YM-75be3", true).getBody().equals("false"));
+  CloseableHttpClient httpClient;
 
-    assertTrue(
-        HttpActionHelper.toGet(new URI(localServerUrl), Action.API_VALIDATION,
-            "?spguid=" + "YM-75be31f6kkp", true).getBody().equals("false"));
+  Set<String> set = newHashSet();
+
+  @Before
+  public void setUp() throws Exception {
+
+    SSLContextBuilder builder = new SSLContextBuilder();
+    builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+    SSLConnectionSocketFactory sslsf =
+        new SSLConnectionSocketFactory(builder.build());
+
+    httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+
+    WorkbookReader reader0 =
+        WorkbookReader.open("./src/main/resources/LegacyPrefixes.xlsx");
+
+    for (List<String> row : reader0.withoutHeader().toLists()) {
+      set.add(row.get(0));
+    }
+
+    set.add("BIOBANK");
+    set.add("YMU");
+    set.add("TEST");
+
   }
 
   @Test
-  public void testExistence() throws URISyntaxException {
+  public void testValidationSubprimeGuidLength()
+      throws URISyntaxException, ClientProtocolException, IOException {
 
-    assertTrue(
-        HttpActionHelper.toGet(new URI(localServerUrl), Action.API_EXISTENCE,
-            "?subprimeGuid=TEST-Y3XZU2NG", true).getBody().equals("true"));
+    HttpGet httpGet = new HttpGet(localServerUrl + "/guids/"
+        + Action.API_VALIDATION + "?spguid=" + "YM-75be3");
+
+    httpGet.addHeader("Content-type", "application/json");
+
+    CloseableHttpResponse postResponse = httpClient.execute(httpGet);
+    HttpEntity entity = postResponse.getEntity();
+
+    assertTrue(IOUtils.toString(entity.getContent()).equals("false"));
+
+    HttpGet httpGet1 = new HttpGet(localServerUrl + "/guids/"
+        + Action.API_VALIDATION + "?spguid=" + "YM-75be31f6kkp");
+
+    httpGet1.addHeader("Content-type", "application/json");
+
+    CloseableHttpResponse postResponse1 = httpClient.execute(httpGet1);
+    HttpEntity entity1 = postResponse1.getEntity();
+
+    assertTrue(IOUtils.toString(entity1.getContent()).equals("false"));
+
+  }
+
+  @Test
+  public void testExistence()
+      throws URISyntaxException, ClientProtocolException, IOException {
+
+    HttpGet httpGet = new HttpGet(localServerUrl + "/guids/"
+        + Action.API_EXISTENCE + "?subprimeGuid=" + "TEST-Y3XZU2NG");
+
+    httpGet.addHeader("Content-type", "application/json");
+
+    CloseableHttpResponse postResponse = httpClient.execute(httpGet);
+    HttpEntity entity = postResponse.getEntity();
+
+    assertTrue(IOUtils.toString(entity.getContent()).equals("true"));
 
   }
 
@@ -66,15 +127,16 @@ public class ApiControllerTest {
   public void testGetAllPrefixListInLocalServerDatabase()
       throws URISyntaxException, IOException {
 
-    Set<String> set = newHashSet();
-    set.add("YMU");
-    set.add("BIOBANK");
-    set.add("TEST");
+    HttpGet httpGet =
+        new HttpGet(localServerUrl + "/guids/" + Action.API_PREFIX);
+
+    httpGet.addHeader("Content-type", "application/json");
+
+    CloseableHttpResponse postResponse = httpClient.execute(httpGet);
+    HttpEntity entity = postResponse.getEntity();
 
     Map<String, Object> flattenJson =
-        JsonFlattener.flattenAsMap(HttpActionHelper
-            .toGet(new URI(localServerUrl), Action.API_PREFIX, "", true)
-            .getBody());
+        JsonFlattener.flattenAsMap(IOUtils.toString(entity.getContent()));
 
     Set<String> s = newHashSet();
 
