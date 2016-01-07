f1555102e64f37c4332f695d3cecb60843c75eac
wmw <wnameless@gmail.com>
2016-01-07 19:51:36 +0800

    Fix SubprimeGuidRepository converter logic mistake

--- src/main/java/tw/guid/local/repository/SubprimeGuidRepository.java
+++ src/main/java/tw/guid/local/repository/SubprimeGuidRepository.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import tw.guid.central.core.PrefixedHashBundle;
 import tw.guid.local.entity.SubprimeGuid;
-import tw.guid.local.security.HashCodeEncryptorHolder;
 
 public interface SubprimeGuidRepository
     extends JpaRepository<SubprimeGuid, Long> {
@@ -18,19 +17,10 @@ public interface SubprimeGuidRepository
   public SubprimeGuid findByHashcode1AndHashcode2AndHashcode3AndPrefix(
       String hashcode1, String hashcode2, String hashcode3, String prefix);
 
-  default public SubprimeGuid findByHashcodesAndPrefix(String hashcode1,
-      String hashcode2, String hashcode3, String prefix) {
-    return findByHashcode1AndHashcode2AndHashcode3AndPrefix(
-        HashCodeEncryptorHolder.getHashCodeEncryptor().encrypt(hashcode1),
-        HashCodeEncryptorHolder.getHashCodeEncryptor().encrypt(hashcode2),
-        HashCodeEncryptorHolder.getHashCodeEncryptor().encrypt(hashcode3),
-        prefix);
-  }
-
   public SubprimeGuid findBySpguid(String spguid);
 
   default public boolean isExist(PrefixedHashBundle prefixedHashBundle) {
-    return findByHashcodesAndPrefix(
+    return findByHashcode1AndHashcode2AndHashcode3AndPrefix(
         prefixedHashBundle.getHash1().substring(0, 128),
         prefixedHashBundle.getHash2().substring(0, 128),
         prefixedHashBundle.getHash3().substring(0, 128),
@@ -39,7 +29,7 @@ public interface SubprimeGuidRepository
 
   default public String getSubprimeGuidBySubprimeGuidRequest(
       PrefixedHashBundle prefixedHashBundle) {
-    return findByHashcodesAndPrefix(
+    return findByHashcode1AndHashcode2AndHashcode3AndPrefix(
         prefixedHashBundle.getHash1().substring(0, 128),
         prefixedHashBundle.getHash2().substring(0, 128),
         prefixedHashBundle.getHash3().substring(0, 128),
