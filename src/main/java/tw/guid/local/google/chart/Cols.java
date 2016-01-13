0b5e684d6189b6571167aa6a54106c9b099a4eff
Ming-Jheng Li <mjli0721@gmail.com>
2016-01-13 23:40:16 +0800

    Add REiMED License and Override

--- src/main/java/tw/guid/local/google/chart/Cols.java
+++ src/main/java/tw/guid/local/google/chart/Cols.java
@@ -1,24 +1,13 @@
-/*
-*
-* @author Ming-Jheng Li
-*
-*
-* Copyright 2015 Ming-Jheng Li
-*
-* Licensed under the Apache License, Version 2.0 (the "License"); you may not
-* use this file except in compliance with the License. You may obtain a copy of
-* the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-* License for the specific language governing permissions and limitations under
-* the License.
-*
-*/
+/* Copyright (c) 2015 ReiMed Co. to present.
+ * All rights reserved.
+ *
+ * @author Ming-Jheng Li
+ *
+ */
 package tw.guid.local.google.chart;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
 
 public final class Cols {
 
@@ -53,6 +42,28 @@ public final class Cols {
    */
   public Type getType() {
     return type;
+  }
+
+  @Override
+  public boolean equals(final Object other) {
+    if (!(other instanceof Cols)) {
+      return false;
+    }
+    Cols castOther = (Cols) other;
+    return Objects.equal(id, castOther.id)
+        && Objects.equal(label, castOther.label)
+        && Objects.equal(type, castOther.type);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id, label, type);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this).add("id", id).add("label", label)
+        .add("type", type).toString();
   }
 
 }
--- src/main/java/tw/guid/local/google/chart/Rows.java
+++ src/main/java/tw/guid/local/google/chart/Rows.java
@@ -1,26 +1,15 @@
-/*
-*
-* @author Ming-Jheng Li
-*
-*
-* Copyright 2015 Ming-Jheng Li
-*
-* Licensed under the Apache License, Version 2.0 (the "License"); you may not
-* use this file except in compliance with the License. You may obtain a copy of
-* the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-* License for the specific language governing permissions and limitations under
-* the License.
-*
-*/
+/* Copyright (c) 2015 ReiMed Co. to present.
+ * All rights reserved.
+ *
+ * @author Ming-Jheng Li
+ *
+ */
 package tw.guid.local.google.chart;
 
 import java.util.List;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
 
 public final class Rows {
 
@@ -35,6 +24,25 @@ public final class Rows {
    */
   public List<Value<?>> getC() {
     return c;
+  }
+
+  @Override
+  public boolean equals(final Object other) {
+    if (!(other instanceof Rows)) {
+      return false;
+    }
+    Rows castOther = (Rows) other;
+    return Objects.equal(c, castOther.c);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(c);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this).add("c", c).toString();
   }
 
 }
